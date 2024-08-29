01c3330e2b19a05d9371109245472406b0f0a5fe
FZ <95338443+febrianzulmi@users.noreply.github.com>
2024-08-28 18:39:11 -0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'aulianza.s3.ap-southeast-1.amazonaws.com',
+      'aulianza.id',
+      'aulianza.com',
+      'dev-to-uploads.s3.amazonaws.com',
+      'res.cloudinary.com',
+      'lh3.googleusercontent.com',
+    ],
+  },
+};
+
+module.exports = nextConfig;
